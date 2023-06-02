951e4e11e5b6ca21d50c3ed81fdb2a88445ead85
zuev.igor <Igor.Zuev@nanoheal.com>
2023-06-02 16:57:14 +0300

    fix solid_d.php

--- architecture/solid_d.php
+++ architecture/solid_d.php
@@ -1,17 +1,36 @@
 <?php
 
-class XMLHttpService extends XMLHTTPRequestService {}
+interface XMLHTTPRequestService
+{
+    public function request(string $url, string $method, array $options = []);
+}
+
+class XMLHttpService implements XMLHTTPRequestService {
+    public function request(string $url, string $method, array $options = [])
+    {
+        echo $url;
+        echo $method;
+        print_r($options);
+    }
+}
 
 class Http {
-    private $service;
+    private XMLHttpService $service;
 
-    public function __construct(XMLHttpService $xmlHttpService) { }
+    public function __construct(XMLHttpService $xmlHttpService) {
+        $this->service = $xmlHttpService;
+    }
 
     public function get(string $url, array $options) {
         $this->service->request($url, 'GET', $options);
     }
 
     public function post(string $url) {
-        $this->service->request($url, 'GET');
+        $this->service->request($url, 'POST');
     }
 }
+
+$http = new Http(new XMLHttpService());
+
+$http->get('localhost', ['Set-Cookie'=> 'cookie-name=success']);
+$http->post('localhost');

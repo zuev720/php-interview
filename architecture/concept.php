f17e36393c1564a1fcec595297536477355328e1
zuev.igor <Igor.Zuev@nanoheal.com>
2023-06-02 17:37:18 +0300

    done getSecretKey() method

--- architecture/concept.php
+++ architecture/concept.php
@@ -1,12 +1,15 @@
 <?php
 class Concept {
-    private $client;
+    private \GuzzleHttp\Client $client;
+    private $storage;
 
     public function __construct() {
         $this->client = new \GuzzleHttp\Client();
+        $this->storage = getenv('STORAGE_TYPE');
     }
 
     public function getUserData() {
+        print_r($this->getSecretKey());
         $params = [
             'auth' => ['user', 'pass'],
             'token' => $this->getSecretKey()
@@ -19,4 +22,23 @@ class Concept {
 
         $promise->wait();
     }
+
+    private function getSecretKey(): string
+    {
+        if ($this->storage === 'File') $secret = 'file';
+        if ($this->storage === 'DB') $secret = 'db';
+        if ($this->storage === 'Redis') $secret = 'redis';
+        if ($this->storage === 'Cloud') $secret = 'cloud';
+
+        $headers_encoded = base64_encode('headers');
+        $payload_encoded = base64_encode(json_encode('$payload'));
+        $signature = hash_hmac('SHA256', "$headers_encoded.$payload_encoded", $secret, true);
+        $signature_encoded = base64_encode($signature);
+        $jwt = "$headers_encoded.$payload_encoded.$signature_encoded";
+
+        return $jwt;
+    }
 }
+
+$concept = new Concept();
+$concept->getUserData();
